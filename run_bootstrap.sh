5bd83afbb6d5541c3b895a554364664cf56d7221
will <zsluedem06@gmail.com>
2019-02-21 12:15:49 +0800

    run bootstrap

---
+++ run_bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+wget https://raw.githubusercontent.com/zsluedem/China-rnode-test-session/master/%E9%80%9A%E7%94%A8/bonds.txt
+wget https://raw.githubusercontent.com/zsluedem/China-rnode-test-session/master/%E9%80%9A%E7%94%A8/wallet.txt
+rnode run -s \
+--prometheus \
+--validator-private-key 901b1f0837b7e891d7c2ea0047f502fd95637e450b0226c39a97d68dd951c8a7 \
+--validator-public-key 00322ba649cebf90d8bd0eeb0658ea7957bcc59ecee0676c86f4fec517c06251 \
+--bonds-file bonds.txt \
+--has-faucet
