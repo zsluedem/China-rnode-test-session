f7dd4edd3b23effc7b8fbbfcff3a149e17c446e8
will <zsluedem06@gmail.com>
2019-03-08 11:57:06 +0800

    2019-03-08

---
+++ 2019-03-08/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install openjdk-11-jdk -y
+wget https://build.rchain-dev.tk/branches/dev/rnode-0.8.4.git550be610.tgz
+tar zxvf rnode-0.8.4.git550be610.tgz
+ln -s $(pwd)/rnode-0.8.4.git550be610.tgz/bin/rnode /usr/bin/rnode
