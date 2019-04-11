b14c4088d54a6beeb0f899f753119120f5c89132
will <zsluedem06@gmail.com>
2019-04-11 10:14:54 +0800

    update install

--- 2019-04-11/install.sh
+++ 2019-04-11/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:linuxuprising/java
 sudo apt-get update
 sudo apt-get install oracle-java11-installer
