eb06891761d0fc236c9098c3363ddb71dac0f3b4
will <zsluedem06@gmail.com>
2019-04-11 10:10:10 +0800

    update install

--- 2019-04-11/install.sh
+++ 2019-04-11/install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install openjdk-11-jdk -y
+sudo apt-get install software-properties-common
+sudo add-apt-repository -y ppa:linuxuprising/java
+sudo apt-get update
+sudo apt-get install oracle-java11-installer
 wget https://github.com/rchain/rchain/releases/download/v0.9.2/rnode-0.9.2.git26ad26eb.tgz
 tar zxvf rnode-0.9.2.git26ad26eb.tgz
-ln -s $(pwd)/rnode-0.9.2.git26ad26eb.tgz/bin/rnode /usr/local/sbin/rnode
+ln -s $(pwd)/rnode-0.9.2.git26ad26eb/bin/rnode /usr/local/sbin/rnode
