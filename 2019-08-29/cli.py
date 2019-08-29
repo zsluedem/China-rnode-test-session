b56f93def3accf338251e45972e7a0b1cbf7422a
Will Qiu <zsluedem06@gmail.com>
2019-08-29 16:07:25 +0800

    2019 08 29

---
+++ 2019-08-29/cli.py
@@ -0,0 +1,64 @@
+import grpc
+import sys
+from rchain.crypto import PrivateKey
+from rchain.client import RClient
+
+from rchain.vault import VaultAPI
+
+import argparse
+
+
+def bond(amount, private_key, grpc_host, grpc_port):
+    key = PrivateKey.from_hex(private_key)
+    with grpc.insecure_channel('{}:{}'.format(grpc_host, grpc_port)) as channel:
+        client = RClient(channel)
+
+        alice_vault_api = VaultAPI(client, key)
+        alice_vault_api.bond(amount=amount)
+
+def check_balance(private_key, grpc_host, grpc_port):
+    key = PrivateKey.from_hex(private_key)
+    with grpc.insecure_channel('{}:{}'.format(grpc_host, grpc_port)) as channel:
+        client = RClient(channel)
+        vault_api = VaultAPI(client, key)
+        balance = vault_api.get_balance()
+
+        print("Current user {} have balance: {}".format(key.get_public_key().get_rev_address(), balance))
+
+def transfer(to_addr, amount, private_key, grpc_host, grpc_port):
+    key = PrivateKey.from_hex(private_key)
+    with grpc.insecure_channel('{}:{}'.format(grpc_host, grpc_port)) as channel:
+        client = RClient(channel)
+        pass
+
+
+def run_cli(args: argparse.Namespace):
+    if args.subcommand == "bond":
+        bond(args.bond_amount, args.private_key, args.grpc_host, args.grpc_port)
+    elif args.subcommand == "check-balance":
+        check_balance(args.private_key, args.grpc_host, args.grpc_port)
+    elif args.subcommand == "transfer":
+        transfer()
+    else:
+        raise NotImplementedError()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog="RChain Cli")
+    parser.add_argument("--grpc-host", type=str, default="127.0.0.1")
+    parser.add_argument("--grpc-port", type=str, default="40401")
+
+    subparsers = parser.add_subparsers(help="subcommand help", dest="subcommand")
+    bond_parser = subparsers.add_parser("bond", help="bond command")
+    bond_parser.add_argument("--bond-amount", type=int)
+    bond_parser.add_argument("--private-key", type=str)
+
+
+    check_balance_parser = subparsers.add_parser("check-balance", help="check balance of a vault")
+    check_balance_parser.add_argument("--private-key")
+
+    transfer_parser = subparsers.add_parser("transfer", help="transfer fund command")
+    transfer_parser.add_argument("--to-addr")
+    transfer_parser.add_argument("--amount")
+
+    args = parser.parse_args()
+    run_cli(args)
---
+++ 2019-08-29/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install -y openjdk-11-jdk
+wget https://github.com/rchain/rchain/releases/download/v0.9.12/rnode-0.9.12.git16d90f43.tgz
+tar zxvf rnode-0.9.12.git16d90f43.tgz
+ln -s $(pwd)/rnode-0.9.12.git16d90f43/bin/rnode /usr/local/bin/rnode
+wget https://raw.githubusercontent.com/zsluedem/China-rnode-test-session/master/%E9%80%9A%E7%94%A8/bonds.txt
+wget https://raw.githubusercontent.com/zsluedem/China-rnode-test-session/master/%E9%80%9A%E7%94%A8/wallets.txt
+
+sudo apt install python3-pip
+sudo pip3 install --upgrade setuptools
+sudo pip3 install git+https://github.com/rchain/pyrchain@0075df7c9b70238056ba52f1505b575d2844e1fc
+
