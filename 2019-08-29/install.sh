87a3132fe11139389e6345d678c17c085ed2cf52
Will Qiu <zsluedem06@gmail.com>
2019-08-29 16:17:07 +0800

    2019 08 29

--- 2019-08-29/install.sh
+++ 2019-08-29/install.sh
@@ -11,7 +11,7 @@ ln -s $(pwd)/rnode-0.9.12.git16d90f43/bin/rnode /usr/local/bin/rnode
 wget https://raw.githubusercontent.com/zsluedem/China-rnode-test-session/master/%E9%80%9A%E7%94%A8/bonds.txt
 wget https://raw.githubusercontent.com/zsluedem/China-rnode-test-session/master/%E9%80%9A%E7%94%A8/wallets.txt
 
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 sudo pip3 install --upgrade setuptools
 sudo pip3 install git+https://github.com/rchain/pyrchain@0075df7c9b70238056ba52f1505b575d2844e1fc
-
+wget https://raw.githubusercontent.com/zsluedem/China-rnode-test-session/master/2019-08-29/cli.py
