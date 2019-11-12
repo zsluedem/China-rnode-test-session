9a9f2b060675440a955f4c65f8e06e4518618f8e
will <you@example.com>
2019-11-12 18:21:36 +0800

    update

--- 2019-11-12-testnet3/install.sh
+++ 2019-11-12-testnet3/install.sh
@@ -3,8 +3,11 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get install -y openjdk-11-jdk
-sudo apt-get install python3-pip
-pip3 install git+https://github.com/rchain/pyrchain
+sudo apt-get install -y python3-pip
+sudo apt-get install -y git
+sudo apt-get -y install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+pip3 install --upgrade setuptools
+pip3 install git+https://github.com/rchain/pyrchain@testnet3
 wget https://github.com/zsluedem/China-rnode-test-session/raw/master/2019-11-12-testnet3/rnode-0.9.16-23-ge8c1050.tgz
 tar zxvf rnode-0.9.16-23-ge8c1050.tgz
 ln -s $(pwd)/rnode-0.9.16-23-ge8c1050/bin/rnode /usr/local/bin/rnode
